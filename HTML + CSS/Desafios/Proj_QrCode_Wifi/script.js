5781455246336b6bd7d97154f12616d9fe85ece4
LukeSkywalker32 <luke.skywalker31@outlook.com>
2025-01-17 08:22:22 -0300

    atualização do js do proj Qr Code

--- HTML + CSS/Desafios/Proj_QrCode_Wifi/script.js
+++ HTML + CSS/Desafios/Proj_QrCode_Wifi/script.js
@@ -1,28 +1,15 @@
-// Seleciona a imagem do QR Code
-const img = document.querySelector('img');
+const img = document.querySelector('img'); // Seleciona a imagem do QR Code
+const ssid = document.querySelector('.ssid');// Seleciona o campo de entrada do SSID (nome da rede)
+const password = document.querySelector('.password');// Seleciona o campo de entrada da senha
+const button = document.querySelector('button');// Seleciona o botão de impressão
 
-// Seleciona o campo de entrada do SSID (nome da rede)
-const ssid = document.querySelector('.ssid');
 
-// Seleciona o campo de entrada da senha
-const password = document.querySelector('.password');
-
-// Seleciona o botão de impressão
-const button = document.querySelector('button');
-
-// Função para atualizar o QR Code com os dados do WiFi
-function update() {
-    // Cria a string de dados do WiFi no formato esperado
-    const wifi = `WIFI:T:WPA;S:${ssid.value};P:${password.value};;`;
-    
-    // Atualiza a fonte da imagem do QR Code com a URL da API
-    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${wifi}`;
+function update() { // Função para atualizar o QR Code com os dados do WiFi    
+    const wifi = `WIFI:T:WPA;S:${ssid.value};P:${password.value};;`;// Cria a string de dados do WiFi no formato esperado     
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${wifi}`;// Atualiza a fonte da imagem do QR Code com a URL da API
 }
 
-// Adiciona um evento de 'keyup' ao campo SSID para atualizar o QR Code ao digitar
-ssid.addEventListener('keyup', update);
-
-// Adiciona um evento de 'click' ao botão para imprimir a página
-button.addEventListener('click', () => {
+ssid.addEventListener('keyup', update);// Adiciona um evento de 'keyup' ao campo SSID para atualizar o QR Code ao digitar
+button.addEventListener('click', () => {   // Adiciona um evento de 'click' ao botão para imprimir a página
     window.print();
 });
