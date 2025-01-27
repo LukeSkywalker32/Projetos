58936c691b8b602242cb76c2806ebc909bf4ad91
LukeSkywalker32 <luke.skywalker31@outlook.com>
2025-01-27 10:11:58 -0300

    add de um bloco de código para melhor ilustrar o operador ternário usado

--- HTML_CSS/Desafios/Proj_Dev_Sorteio/script.js
+++ HTML_CSS/Desafios/Proj_Dev_Sorteio/script.js
@@ -4,21 +4,31 @@ const SubmitButton = document.querySelector(".button-sorty")
 const Resultado = document.querySelector(".resultado")
 
 
-
-
 function sorty() {
     const min = parseInt(MinInput.value, 10);
     const max = parseInt(MaxInput.value, 10);
-    
+
     if (isNaN(min) || isNaN(max)) {
         alert("Insira valores válidos!");
         return;
     }
+
+    const [valorMin, valorMax] = min > max ? [max, min] : [min, max]; // essa linha é o mesmo que o bloco de código abaixo
     
-    const [valorMin, valorMax] = min > max ? [max, min] : [min, max];
+    /*
+    const valorMin, valorMax;
     
+    if (min > max) {
+        valorMin = max;
+        valorMax = min;
+    } else {
+        valorMin = min;
+        valorMax = max;
+    }
+    */
+
     const result = Math.floor(Math.random() * (valorMax - valorMin + 1)) + valorMin;
-        
+
     Resultado.innerHTML = result;
     Resultado.style.opacity = 0;
 
@@ -26,7 +36,7 @@ function sorty() {
         Resultado.style.opacity = 1;
     }, 10);
 
-//    alert(`O número sorteado foi: ${result}`);
-} 
+    //    alert(`O número sorteado foi: ${result}`);
+}
 
 SubmitButton.addEventListener("click", sorty)
