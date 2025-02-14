42acdda8fe5237db936286c10951ca701b7c37e1
LukeSkywalker32 <luke.skywalker31@outlook.com>
2025-02-13 23:36:45 -0300

    continue node

--- HTML_CSS/Aulas/NODE/server.js
+++ HTML_CSS/Aulas/NODE/server.js
@@ -3,6 +3,34 @@
 
 // NOVO
 import express from 'express';
-
 const app = express();
+app.use(express.json());
+
+
+//TIPOS DE ROTAS
+// GET = pegar
+app.get('/usuarios', (req, res) => {
+    console.log(req)
+    res.send('Resposta do servidor é essa para tipo GET: Hello World!')
+})
+// req = request
+// res = response
+
+// POST = enviar
+app.post('/usuarios', (req, res) => {
+    res.send('Resposta do servidor é essa para tipo POST: Hello World!')
+})
+
+//ROUTE PARAMS
+app.get("/usuarios/:itemVarivel", (req, res) => {
+    console.log(req);
+    res.send("Resposta do servidor é essa para tipo GET/ROUTE PARAMS");
+});
+
+app.post("/usuariosBody", (req, res) => {
+    console.log(req);
+  res.send("Resposta do servidor é essa para tipo POST/BODYPARAMS: Hello World!");
+});
+
+
 app.listen(3000)
