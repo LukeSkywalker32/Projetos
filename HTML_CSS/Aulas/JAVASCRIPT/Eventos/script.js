c288b8634320b1b916cee94ab7e153def44d861c
LukeSkywalker32 <luke.skywalker31@outlook.com>
2025-04-16 08:53:56 -0300

    testes

--- HTML_CSS/Aulas/JAVASCRIPT/Eventos/script.js
+++ HTML_CSS/Aulas/JAVASCRIPT/Eventos/script.js
@@ -7,7 +7,7 @@ const input = document.querySelector('#main-input') // aqui eu mapeio o meu inpu
 const tag_alvo = document.querySelector('#tag_alvo')// aqui eu mapeio o minha tag p vazia do HTML
 
 const button = document.querySelector('.main-button')// aqui eu mapeio o meu botão
-button.style.fontSize = "30px"
+button.style.fontSize = "20px"
 button.style.fontWeight = "bold"
 button.style.padding = "10px"
 button.style.borderRadius = "20px"
@@ -15,13 +15,11 @@ button.style.border = "none"
 button.style.cursor = "pointer"
 
 
-
-
 // Atualiza o texto, tamanho, cor, fonte e peso do elemento alvo com os valores do input
 function cliquei (){ 
     tag_alvo.innerText = input.value 
     
-    tag_alvo.style.fontSize = "100px"
+    tag_alvo.style.fontSize = "80px"
     tag_alvo.style.color = "#E1986E"
     tag_alvo.style.fontFamily = "Arial"
     tag_alvo.style.fontWeight = "bold"
