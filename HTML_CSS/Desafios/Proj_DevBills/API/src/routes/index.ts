3200e6586322e1591609bf41faf72b4933082dd7
Luke Skywalker <luke.skywalker31@outlook.com>
2025-06-28 13:35:11 -0300

    projeto devbills

---
+++ HTML_CSS/Desafios/Proj_DevBills/API/src/routes/index.ts
@@ -0,0 +1,14 @@
+import type { FastifyInstance } from "fastify";
+
+
+async function routes(fastify: FastifyInstance):Promise<void> {
+
+    fastify.get('/health', async() => {
+        return {
+            status: 'ok',
+            message: "API rondando normalmente"
+        }
+    })
+
+}
+export default routes
