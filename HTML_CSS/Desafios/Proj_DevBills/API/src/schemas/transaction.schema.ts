35f7d679517d122eec323071a8e2eb6e3de9a460
Luke Skywalker <luke.skywalker31@outlook.com>
2025-07-21 23:35:36 -0300

    feat: add transaction schema and MongoDB integration
    This commit introduces the following changes:
    
    - Adds `mongodb` and `zod` as dependencies.
    - Creates `src/schemas/transaction.schema.ts` to define the schema for transaction data using Zod for validation. This schema includes validation for description, amount, date, categoryId, and type.

---
+++ HTML_CSS/Desafios/Proj_DevBills/API/src/schemas/transaction.schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { ObjectId } from "mongodb";
+import { TransactionType } from "@prisma/client";
+
+const isValidObjectId = (id: string): boolean => {
+  return ObjectId.isValid(id);
+};
+
+export const createTransaction = z.object({
+  description: z.string().min(3, "Descrição Obrigatória"),
+  amount: z.number().positive("Valor deve ser positivo"),
+  date: z.coerce
+    .date()
+    .refine((val) => val instanceof Date && !isNaN(val.getTime()), {
+      message: "Data inválida",
+    }),
+  categoryId: z.string().refine(isValidObjectId, {
+    message: "Categoria inválida",
+  }),
+  type: z.enum([TransactionType.expense, TransactionType.income], {
+    error: "Data inválida",
+  }),
+});
+
+
+export default createTransaction;
